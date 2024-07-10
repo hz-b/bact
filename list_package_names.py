0d2ae9d97a8443232096267239b19ef20bc010b6
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-07-10 18:07:10 +0200

    [FIX] dirs listed line by line: simplifies not installing archiver

--- list_package_names.py
+++ list_package_names.py
@@ -17,51 +17,14 @@ def find_package_dirs(t_dir: str):
     for file_path in setup_files:
         package_dirs.add(os.path.dirname(file_path))
 
-    return list(package_dirs)
-
-
-class PackageKey:
-    """Priorise base packages
-
-    Hand coded what dependencies we have along the line
-    """
-    def __init__(self):
-        self.counter = count()
-        self.core_custom_values = dict(core=1000, custom=5000)
-        self.core_values   = {'math-utils' : -900, 'device-models' : -800}
-        self.custom_values = {'ophyd' : -900, 'bluesky' : -800}
-
-    def __call__(self, package_dirs: str):
-        root, *t_dirs = package_dirs.split('/')
-        val = self.core_custom_values[root]
-        if root == 'core':
-            pkg_name, = t_dirs
-            try:
-                val += self.core_values[pkg_name]
-            except KeyError:
-                pass
-        if root == 'custom':
-            L = len(t_dirs)
-            if L == 1:
-                # packages before specific machines
-                val -= 4000
-            elif L == 2:
-                pkg_name = t_dirs[1]
-                try:
-                    val += self.custom_values[pkg_name]
-                except KeyError:
-                    pass
-
-        return val #+ next(self.counter)
+    return package_dirs
 
 
 def main():
-    """Traverse directories executing install for appropriate ones"""
-    package_dirs = list(find_package_dirs("core/") + find_package_dirs("custom/"))
-    package_dirs.sort()
-    # package_dirs.sort(key=PackageKey())
+    """Traverse directories listing the suitable ones"""
+    package_dirs = list(find_package_dirs("core/")) + list(find_package_dirs("custom/"))
 
-    print(" ".join([f"{t_dir}/" for t_dir in package_dirs]))
+    print("\n".join([f"{t_dir}/" for t_dir in package_dirs]))
 
 
 if __name__ == "__main__":
